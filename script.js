e941134ac7f3daa8248d382efdf23287eb22d691
Rizwan khan <141172031+85Rizwan@users.noreply.github.com>
2024-12-04 18:03:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function highlight() {
     //Write your code here
-
+let strongs = document.querySelectorAll("strong");
+// strongs.style.background = "green"
+										
+strongs.forEach((str) => {
+		str.style.color = "green"
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
+	let strongs = document.querySelectorAll("strong");
+										
+strongs.forEach((str) => {
+		str.style.color = "black"
+	})
 
     
 }
